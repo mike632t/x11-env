369c4640654d5e8de430f321bbd50d72864b8166
mike632t <mike632t@gmail.com>
2024-02-11 20:56:17 +0000

    Updated licence

--- x11-env.c
+++ x11-env.c
@@ -8,20 +8,19 @@
  * vairables  on  the screen before waiting for the user to press a key  or 
  * click in the window.  
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This  program is free software: you can redistribute it and/or modify it
+ * under  the terms of the GNU General Public License as published  by  the
+ * Free  Software Foundation, either version 3 of the License, or (at  your
+ * option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
- * GNU General Public License for more details.
+ * This  program  is distributed in the hope that it will  be  useful,  but
+ * WITHOUT   ANY   WARRANTY;   without even   the   implied   warranty   of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.   If not, see <http://www.gnu.org/licenses/>.
- * 
- * Displays some text in a window on the screen.
+ * You  should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  *
  * On VMS:
  * Compile with 'cc x11-env' 
@@ -30,7 +29,7 @@
  * On Linux:
  * Compile and link with 'gcc x11-env.c -o x11-env -lX11'
  *
- * 11 Feb 24   0.1      - Initial version - MT
+ * 11 Feb 24   0.1   - Initial version - MT
  *
  */
 
